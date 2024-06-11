c666cf84c7fdec9744dd146cef4e7ed6d6df6e59
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-06-11 10:05:20 -0300

    Initial Commit

---
+++ IMC-CALCULATOR/IMC_CALCULATORApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMC_CALCULATORApp.swift
+//  IMC-CALCULATOR
+//
+//  Created by Yasmin Carloto on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IMC_CALCULATORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
