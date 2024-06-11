884d212edbf1e4319c525b176f393e8810f711f4
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-06-11 11:42:57 -0300

    projeto concluído

--- IMC-CALCULATOR/ContentView.swift
+++ IMC-CALCULATOR/ContentView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var weight: String = ""
+    @State var height: String = ""
+    @State var imcResult: String = "Informe seus dados!"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading){
+            Label("Calculadora IMC", image: "globe")
+                .labelStyle(.titleOnly)
+                .font(.title2)
+                .bold()
+                .multilineTextAlignment(.leading)
+            
+            VStack{
+                TextField("Peso (Kg)", text: $weight)
+                    .padding(.vertical, 10)
+                    .padding(.bottom, 10)
+                    .overlay{
+                        Divider()
+                            .background(Color.mint)
+                            .offset(x: 0, y: 15)
+                        }
+                
+                TextField("Altura (cm)", text: $height)
+                    .padding(.vertical, 10)
+                    .padding(.bottom, 10)
+                    .overlay(VStack{
+                        Divider()
+                            .background(Color.mint)
+                            .offset(x: 0, y: 15)
+                    })
+            }
+            .padding(.top, 30)
+            .padding(.bottom, 30)
+            
+            Button("Calcular", systemImage: "arror.up") {
+                if let heightNumber = Float(height), let weightNumber = Float(weight){
+                    let doubleHeight = heightNumber*heightNumber
+                    let imc = weightNumber/doubleHeight
+                    imcResult = "O resultado do seu IMC é \(String(format: "%.2f", imc))"
+                }else{
+                    imcResult = "Você precisa preencher todos os campos antes de calcular o IMC."
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(10)
+            .background(Color.mint)
+            .cornerRadius(20)
+            .foregroundColor(Color.black)
+
+            Text(imcResult)
+                .frame(maxWidth: .infinity)
+                .padding(20)
+                .foregroundColor(Color.mint)
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .frame(height: 800)
         .padding()
     }
 }
